c775c14aebcfc47b3dea1240e1e7dc9bce272b3c
camilohdez_Portatil <camilohdez@gmail.com>
2023-08-23 20:25:41 -0500

    UnitOfWork

--- Domain/Interfaces/IUnitOfWork.cs
+++ Domain/Interfaces/IUnitOfWork.cs
@@ -2,6 +2,13 @@ namespace Domain.Interfaces;
 
 public interface IUnitOfWork{
 
-    
+    ICityRepository Country {get;}
+    IClassroomRepository Classroom {get;}
+    IGenderRepository Gender {get;}
+    IPersonRepository Person {get;}
+    IRegionRepository Region {get;}
+    ITrainerClassroomRepository TrainerClassroom {get;}
+    ITuitionRepository Tuition {get;}
+    ITyperPersonRepository TyperPerson {get;}
 
 }
